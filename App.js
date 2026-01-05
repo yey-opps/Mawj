aed0c810afe19b86c93bdbc491e0870b794e039c
yey-ops <rhari.yasmina@gmail.com>
2026-01-05 16:43:03 +0100

    interface avec api

---
+++ App.js
@@ -0,0 +1,15 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { AppProvider } from './src/context/AppContext';
+import AppNavigator from './src/navigation/AppNavigator';
+
+export default function App() {
+    return (
+        <AppProvider>
+            <NavigationContainer>
+                <AppNavigator />
+            </NavigationContainer>
+        </AppProvider>
+    );
+}
